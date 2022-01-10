2f066476400ac970077d3dcc68204b9bcdd56d87
cfred22 <icfred@me.com>
2022-01-10 16:39:16 +0100

    kanaps vignettes  ok

--- front/js/index.js
+++ front/js/index.js
@@ -1,21 +1,27 @@
 const url = 'http://localhost:3000/api/products';
-let kanaps = "()";
+var kanaps = "";
 let section = document.getElementById('items'); // variable 'section' pour aller chercher ID items
+var html = "";
 
 //API request
-fetch(url) // va chercher l'url API, 
+fetch(url) // va chercher l'url API, requete HTTP 
     .then(resp => resp.json())
     .then(function(data) {    // ensuite renvoie la reponse converti en JSON
         kanaps = data;
         console.log(data[2].name);  // essai console recuperation name ok dans le tableau
-        section.innerHTML = "<img src='" + data[2].imageUrl + "'>"; // j'injecte une image html (exemple) 
+        kanaps.forEach((data => {
+            html = html +
+            `<a href="./product.html?id=${data._id}">
+                <article>
+                    <img src="${data.imageUrl}" alt="${data.altTxt}">     
+                    <h3 class="productName">${data.name}</h3>
+                    <p class="productDescription">${data.description}</p>
+                </article>
+            </a>`;
+        section.innerHTML = html;}))
+        
     });
-
-
-
- 
-
-    
+;
 
 
 
@@ -24,7 +30,7 @@ fetch(url) // va chercher l'url API,
 
 
 
-
+//section.innerHTML = "<img src='" + data[2].imageUrl + "'>"; // j'injecte une image html (exemple) 
 
 
 
