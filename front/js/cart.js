a0119ac709d4d67a9e187676e10c6c9b5342370c
cfred22 <icfred@me.com>
2022-01-19 23:46:56 +0100

    incrémentation d'un même produit sur le localstorage

--- front/js/cart.js
+++ front/js/cart.js
@@ -22,20 +22,20 @@ panier.forEach(item => {
     .then(resp => resp.json())
     .then(kanap => {
         html = html + `
-          <article class="cart__item" data-id="${kanap._id} data-color="${kanap.colors}">
+          <article class="cart__item" data-id="${kanap._id} data-color="${item.colors}">
             <div class="cart__item__img">
                 <img src="${kanap.imageUrl}" alt="${kanap.altTxt}">
             </div>
             <div class="cart__item__content">
                   <div class="cart__item__content__description">
                     <h2>${kanap.name}</h2>
-                    <p>Vert</p>
+                    <p>${item.colors}</p>
                     <p>${kanap.price} €</p>
                   </div>
                   <div class="cart__item__content__settings">
                     <div class="cart__item__content__settings__quantity">
                       <p>Qté : </p>
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${kanap.quantity}">
+                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${item.quantity}">
                     </div>
                     <div class="cart__item__content__settings__delete">
                       <p class="${kanap.delete}">Supprimer</p>
--- front/js/product.js
+++ front/js/product.js
@@ -12,120 +12,118 @@ const recupId = url.searchParams.get("id");
 console.log(recupId);                        // test ok
 
 // Création des constantes du produit kanap
-const panier = document.getElementById("addToCart");
+const boutonPanier = document.getElementById("addToCart");
 //console.log(panier);
+
+ // Intègration du html via la class ou l'id
 const picKanap = document.querySelector(".item__img");
 const nomKanap = document.getElementById("title");
 const prixKanap = document.getElementById("price");
 const descriptionKanap = document.getElementById("description");
-const couleurKanap = document.getElementById("colors");
+const kanapCouleur = document.getElementById("colors");
 const kanapQuantity = document.getElementById("quantity");
 
 // Création des variables du produit kanap
 var kanap = new Object();
-// Variable liée aux event 
-let quantity;
-let colors;
- 
-
-
+// Variable liée au event 
+var quantity;
+var colors;
      
 // Récup. détails de tout le produit depuis l'API
 fetch(`http://localhost:3000/api/products/${recupId}`)
     .then(response => response.json())
     .then(kanap => {    
-        picKanap.innerHTML =                // Intègration de l'image via la class
+        picKanap.innerHTML =                
         `<img id="image" src="${kanap.imageUrl}" alt="${kanap.altTxt}">`;  
-        nomKanap.innerHTML =                // Intègration du Nom via l'id
+        nomKanap.innerHTML =                
         `<h1 id="title">${kanap.name}</h1>`;
-        prixKanap.innerHTML =               // Intègration du Prix via l'id
+        prixKanap.innerHTML =               
         `<span id="price">${kanap.price}</span>`; 
-        descriptionKanap.innerHTML =             // Intègration description via l'id
-        `<p id="description">${kanap.description}</p>`;     
+        descriptionKanap.innerHTML =             
+        `<p id="description">${kanap.description}</p>`;  
 
-        let couleurs = kanap.colors;        // création variable couleurs                                         
-        couleurs.forEach((couleurs => {     // Fonction Ajout des couleurs du Kanap  
-            couleurs = couleurKanap.innerHTML +=   // Intègration couleurs via l'id
-            `<option value="">${couleurs}</option>`; 
+        // création variable couleurs  
+        let couleurs = kanap.colors; 
+        // Fonction Ajout des couleurs du Kanap                                                
+        couleurs.forEach((couleurs => { 
+            // Intègration couleurs via l'id    
+            couleurs = kanapCouleur.innerHTML +=   
+            `<option value="${couleurs}">${couleurs}</option>`; 
         })
     )    
 });
 
 // Event pour la liste déroulante du choix de couleurs
-couleurKanap.addEventListener("change", (event) => {
+kanapCouleur.addEventListener("change", (event) => {
     colors = event.target.value;
-    console.log(colors); 
-})
+    console.log(event.target); 
+});
 
-// selection du Nombre d'articles
+// Event pour selection du Nombre d'articles 
 kanapQuantity.addEventListener("change", (event) => {
-    quantity = event.target.value;
+    quantity = parseInt(event.target.value);
     console.log(quantity); 
-        
-})
+});
 
-
-// Au moment du clic stockage dans local storage 
-panier.addEventListener("click", (event) => {
+// Au moment du clic stockage dans le local storage 
+boutonPanier.addEventListener("click", (event) => {
     event.preventDefault();
     // Condition du click sur quantité !
-    if(quantity > 0 && quantity <= 100 )  {
+    if(quantity > 0 && quantity <= 100 && colors !== undefined )  {
         const urlSearchParams = new URLSearchParams(window.location.search);
         const params = Object.fromEntries(urlSearchParams.entries());  // recupère l'id dans l'URL
-        console.log(params);   
+        //console.log(params);   
         kanap.id = params.id;
         kanap.quantity = quantity;
+        kanap.colors = colors;
+        // Ajout Panier -> voir fonction (Ajouter des produits !)  
         ajoutPanier(kanap);       
 
     } else {   
-        alert('Choisissez une couleur et une quantité inférieur à 100 articles !')
+        alert('Choisissez une couleur et une quantité \(inférieur à 100 articles !\)')
         return
 }})
 
+// Si panier vide 
+function getPanier() {
+    let panier = localStorage.getItem("panier");
+        if (panier < 1) {  
+        return[];         
+    } else {
+        return JSON.parse(panier);
+    };
+}
 
+// Ajouter des produits ! 
+function ajoutPanier(kanap) {
+    //recupère le tableau panier du localstorage 
+    let panier = getPanier(); 
+    // Création d'une variable "je suis la!"    
+    let isHere = false;
+    // Pour chaque kanap du panier localstorage comparaison 
+    panier.forEach(item => {
+        if(item.id == kanap.id && item.colors == kanap.colors) {
+            // Si il y a un kanap (id + couleur) identique alors on incrémente le localstorage
+            isHere = true;
+            item.quantity += parseInt(kanap.quantity);
+        }  
+    });
+    // Si il n'y a pas de kanap (id + couleur) identique alors on ajoute un clé 
+    if (isHere == false) {
+        panier.push(kanap);  
+    }
+    // sauvegarde du panier -> voir fonction (sérialisation plus bas) 
+    savePanier(panier);     
+};
 
 // Serialisation qui transforme une donnée complexe en chaine de caractère.  
 function savePanier(panier) {                               
     localStorage.setItem('panier', JSON.stringify(panier)); 
 }
 
-// Si panier vide 
-function getPanier() {
-    let panier = localStorage.getItem("panier");
-    if (panier < 1) {  
-        return[];   // Panier vide       
-    } else {
-        return JSON.parse(panier);
-    };
-}
-
-// Ajouter des produits (kanaps)
-function ajoutPanier(kanap) {
-    let panier = getPanier();
-    let foundKanap = panier.find(k => k.id == kanap.id); // Cherche si id(kanap) déjà existant dans mon panier
-        if (foundKanap != undefined) {  
-        alert("Vous ajoutez le même canapé ?! visitez votre panier et modifier votre quantité ;)");
-        return;
-    }else{
-        kanapQuantity;
-        
-    }  
-    panier.push(kanap);  // Ajouter des produits (Panier considéré comme un tableau)
-    savePanier(panier);     
-}
 
 
 
-// Au moment du clic stockage dans local storage 
-/*panier.addEventListener("click", (event) => {
-    const urlSearchParams = new URLSearchParams(window.location.search);
-    const params = Object.fromEntries(urlSearchParams.entries());  // recupère l'id dans l'URL
-    console.log(params);
-    if (kanapQuantity > 0 && kanapQuantity <= 100) { 
-        kanap.id = params.id;
-        kanap.quantity = quantity;
-        ajoutPanier(kanap);
-    } else {   
-        alert('choisissez une couleur et une quantité: ' + err)}
-        return
-})*/
+
+
+
